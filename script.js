577bb98dee7184828862b13551b8b62d5a64485d
l.kalina <kalinaluba794@gmail.com>
2023-12-05 22:58:28 +0400

    lesson06

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 'use strict'
+const gameNumber = function() {
+    
+    const num = Math.floor(Math.random() * 100);
+    let numUser = prompt('Угадай число от 1 до 100')
 
-console.log(';jgf')
+    if (num > numUser) {
+        alert ('Загаданное число больше')
+        numUser = prompt('Введи новый вариант')
+    }
+    else if (num < numUser) {
+        alert('Загаданное число меньше')
+        numUser = prompt('Введи новый вариант')
+    }
+    else if (numUser === null) {
+        confirm('Игра окончена')
+        return
+    }
+    confirm('Поздравляю, Вы угадали!!!')
+}
+gameNumber()
